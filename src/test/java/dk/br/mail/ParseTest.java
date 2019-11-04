f364f4082331c61f3838b966a7a0fd602059596d
Ole Sandum <ole@sandum.net>
2019-11-04 15:56:52 +0100

    BR-2708 exposed

--- src/test/java/dk/br/mail/ParseTest.java
+++ src/test/java/dk/br/mail/ParseTest.java
@@ -5,7 +5,10 @@ import java.net.URL;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import static junit.framework.Assert.*;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -18,29 +21,31 @@ public class ParseTest
 {
   private final static Logger LOG = LoggerFactory.getLogger(ParseTest.class);
   private final static DocumentBuilderFactory DBF = DocumentBuilderFactory.newInstance();
-  
+
   private DocumentBuilder db;
-    
+
+  @Test
   public void testGoodMail() throws IOException {
     testMailParser("OnlineOverbidNotification-mail.xml");
     testMailParser("OnlineOverbidNotification-mail_ws.xml");
+    testMailParser("mail-merge-sample.xml");
   }
-  
+
   private void testMailParser(String resource) throws IOException {
-    assertNotNull(resource);    
-    
+    assertNotNull(resource);
+
     URL xmlSource = getClass().getResource(resource);
-    assertNotNull(xmlSource);    
-    LOG.info("read {}", xmlSource);    
-    
+    assertNotNull(xmlSource);
+    LOG.info("read {}", xmlSource);
+
     Document doc = parse(xmlSource);
     assertNotNull(doc);
-    
+
     MailMessageData[] mailData = MailMessageParser.parseMails(doc);
     assertNotNull(mailData);
-    LOG.info("read {}", mailData);    
+    LOG.info("read {}", mailData);
   }
-    
+
   private Document parse(URL src) throws IOException {
     try {
       db = DBF.newDocumentBuilder();
@@ -50,7 +55,7 @@ public class ParseTest
     }
 
     try {
-      return db.parse(src.openStream());    
+      return db.parse(src.openStream());
     }
     catch (SAXException ex) {
       throw new RuntimeException(ex);
